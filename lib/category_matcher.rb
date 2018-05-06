2a81ecaa604454eb483513cff442f70f4dc78180
jkimmeyer <jkimmeyer@googlemail.com>
2018-05-07 01:06:53 +0200

    Refactor category matcher

--- lib/category_matcher.rb
+++ lib/category_matcher.rb
@@ -2,7 +2,6 @@ module CategoryMatcher
   # needs the group hash of a Dataset
   def get_groups_of_dataset(group)
     hash = Hash.new
-
     # Word Arrays to match the depending category. Key is used to be displayed and to be saved to dataset.
     hash["agri"] = %w[landwirtschaft fischerei forstwirtschaft nahrungsmittel agri agriculture]
     hash["envi"] = %w[envi umwelt environment klima]
@@ -14,16 +13,25 @@ module CategoryMatcher
     hash["tran"] = %w[verkehr transport tran transportation infrastruktur]
     hash["econ"] = %w[wirtschaft finanzen econ economy]
     hash["tech"] = %w[wissenschaft technologie tech]
-    hash["regi"] = %w[regi regierung verwaltung politik wahlen stadt]
+    hash["regi"] = %w[regi regierung verwaltung politik wahlen]
     hash["geo"]  = %w[geo map geographie karte ]
 
-    #Iterate through every hash for the group of a dataset
+    # filter just specific values out of the group and category key
+    group.values.flatten!.each do |g|
+      g.slice!('description', 'title', 'name', 'display_name')
+    end
+
+    @categories = Array.new
     hash.each do |key, values|
       # Turns the group hash into a string with words(downcase) like this: "name title description display_name" so its easier to match with the words that fit.
-      if values.any? {|value| group.values_at("name", "title", "description", "display_name").join(" ").downcase.include? value}
-        # Returns key, if any value includes a word of the group. Can return multiple values.
-        return key
+      if values.any? {|value| group.flatten.join(" ").downcase.include? value}
+        @categories << key
       end
+    end
+    if @categories.blank?
+      return ['other']
+    else
+      return @categories
     end
   end
 end
